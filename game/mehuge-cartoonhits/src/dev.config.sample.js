7ebc3ae7e85dafc8ce7c4fdd2456bd7ca27968b8
Mehuge <adf@sorcerer.co.uk>
2018-07-08 13:58:09 +0100

    Add support for dev.config.js

---
+++ game/mehuge-cartoonhits/src/dev.config.sample.js
@@ -0,0 +1,8 @@
+window.cuOverrides = {
+  loginToken: 'your-login-token',
+  characterID: 'your-character-id',
+  apiHost: 'https://hatchery-api.camelotunchained.com',
+  apiVersion: 1,
+  shardID: 1,
+  debug: true
+};
